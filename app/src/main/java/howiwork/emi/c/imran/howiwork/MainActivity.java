6fef2852e58733742b173cb36857280623c89ff6
Md. Imran Choudhury <imrankst1221@gmail.com>
2016-08-14 08:37:11 +0600

    Home UI improve

--- app/src/main/java/howiwork/emi/c/imran/howiwork/MainActivity.java
+++ app/src/main/java/howiwork/emi/c/imran/howiwork/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         viewPager = (ViewPager) findViewById(R.id.tab_pager);
